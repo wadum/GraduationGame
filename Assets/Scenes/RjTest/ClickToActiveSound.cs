72b48e93eed68b83bcff9f49b3e215e4af71b7dc
Benjamin <asdf@gmail.com>
2015-10-26 14:18:16 +0100

    sound stuff lolol

---
+++ Assets/Scenes/RjTest/ClickToActiveSound.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToActivateSound : MonoBehaviour
+{
+
+    AudioSource sound;
+
+    // Use this for initialization
+    void Start()
+    {
+        sound = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0) == true)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit, Mathf.Infinity);
+
+            if (hit.transform.gameObject.tag == "PlayOnClick")
+            {
+                sound.Play();
+            }
+        }
+
+   /* void OntriggerEnter(Collider player)
+    {
+        if (player.tag != "Player")
+        {
+            return;
+        }
+        sound.Play();*/
+    }
+}
---
+++ Assets/Scenes/RjTest/TriggerSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerSound : MonoBehaviour {
+
+    AudioSource sound;
+
+    // Use this for initialization
+    void Start () {
+        sound = this.gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OntriggerEnter (Collider player)
+    {
+        if (player.tag != "Player")
+        {
+            return;
+        }
+        sound.Play();
+    }
+}
