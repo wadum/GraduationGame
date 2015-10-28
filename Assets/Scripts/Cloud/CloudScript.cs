261d0c3ce94d9666a581bc200837e14342b43fe3
Martin <kullax@feral.dk>
2015-10-28 14:34:49 +0100

    Cloud was missing script for the wind.

---
+++ Assets/Scripts/Cloud/CloudScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudScript : MonoBehaviour {
+    public GameObject wind;
+    public Vector3 WindDirection;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("SpawnWind", 0.5f, 0.5f);
+    }
+
+  
+    void SpawnWind()
+    {
+        GameObject la = (GameObject)Instantiate(wind, transform.position, Quaternion.identity);
+        la.GetComponent<ObjectMoveScript>().SetDirection(WindDirection);
+    }
+}
