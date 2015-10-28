4c6ebaa5be8f44c70be28f7955340f1e3b8c8164
ChristofferL <chrwadum@gmail.com>
2015-10-28 15:22:48 +0100

    Simple pause menu to restart the level.

--- Assets/Scripts/GameOverlay/GameOverlayController.cs
+++ Assets/Scripts/GameOverlay/GameOverlayController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GameOverlayController : MonoBehaviour {
 
 	public GameObject StoryText;
+	public GameObject PauseMenu;
 
 	private Canvas _canvas;
 
@@ -13,9 +14,31 @@ public class GameOverlayController : MonoBehaviour {
 		_canvas.worldCamera = Camera.main;
 	}
 
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+			TogglePauseMenu();
+	}
+
 	public void ShowStoryText(string textFile)
 	{
 		StoryText.SetActive(true);
 		StoryText.GetComponent<StoryTextController>().Show(textFile);
+	}
+
+	public void TogglePauseMenu()
+	{
+		if(PauseMenu.activeSelf) HidePauseMenu();
+		else ShowPauseMenu();
+	}
+
+	public void ShowPauseMenu()
+	{
+		PauseMenu.SetActive(true);
+	}
+
+	public void HidePauseMenu()
+	{
+		PauseMenu.SetActive(false);
 	}
 }
