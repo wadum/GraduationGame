50e8a41f7b9ed0f637fd92cecbe83a51af455513
ChristofferL <chrwadum@gmail.com>
2015-10-28 12:48:06 +0100

    First iteration of a story text controller. Contains the first iteration of a GameOverlay too.

---
+++ Assets/Scripts/GameOverlay/GameOverlayController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverlayController : MonoBehaviour {
+
+	public GameObject StoryText;
+
+	private Canvas _canvas;
+
+	void Start()
+	{
+		_canvas = GetComponentInChildren<Canvas>();
+		_canvas.worldCamera = Camera.main;
+	}
+
+	public void ShowStoryText(string textFile)
+	{
+		StoryText.SetActive(true);
+		StoryText.GetComponent<StoryTextController>().Show(textFile);
+	}
+}
