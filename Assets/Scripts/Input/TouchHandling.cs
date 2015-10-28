8dcd163a1fca4d3ae7b95471685e502802741130
Thorbjørn S. Kaiser <lazareth@gmail.com>
2015-10-28 23:00:26 +0100

    implemented a system for registering handlers for taps and gestures.
    * The input script can be temporarily silenced simply by disabling it, and enabling it again will return it to full functionality.
    * Right now it only supports one handler for swipe, one for gestures, and the tap handlers depending on game object tag.
    * It is not aware of concepts such as zoom level or other game states.
    ** This can either be handled in the handlers on a case-by-case basis, or the dictionary could be depend on a combination of a layer/zoom level and tag.
    ** A third option could be to have different TouchHandling scripts for each level, and switching between them by enabling and disabling them.
    ** Alternatively firing the different types of handlers could be silenced temporarily by implementing methods such as EnableTaps and DisableTaps, etc, to optimize the raycast away if need be.
    
    Ideas for simplifying usage:
    * Introduce abstract class, such as Tappable, which handles registration and exposes a method to be implemented such as "OnTap(RaycastHit)".

--- Assets/Scripts/Input/TouchHandling.cs
+++ Assets/Scripts/Input/TouchHandling.cs
@@ -1,34 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TouchHandling : MonoBehaviour
-{
-    void Start()
-    {
+public class TouchHandling : MonoBehaviour {
+    private Dictionary<string, Action<RaycastHit>> _tapEventHandlers;
+    private Action<Touch> _swipeHandler;
+    private Action<List<Touch>> _pinchHandler;
+
+    void Start() {
+        _tapEventHandlers = new Dictionary<string, Action<RaycastHit>>();
+        ClearSwipeHandler();
+        ClearPinchHandler();
+    }
+
+    void OnEnable() {
         StartCoroutine(AwaitInput());
     }
 
-    private static List<Touch> GetTouches()
-    {
-        return Input.touches.Where(t => EventSystem.current.IsPointerOverGameObject(t.fingerId)).ToList();
-    } 
+    void OnDisable() {
+        StopAllCoroutines();
+    }
 
-    private IEnumerator AwaitInput()
-    {
-        while (true)
-        {
+    #region State machine for recognizing multitouch taps and gestures
+    private IEnumerator AwaitInput() {
+        while (true) {
             var touches = GetTouches();
-            if (touches.Count == 1 && touches[0].phase == TouchPhase.Ended)
-            {
+            if (touches.Count == 1 && touches[0].phase == TouchPhase.Ended) {
                 HandleTap(touches[0]);
                 break;
             }
 
-            if (touches.Count > 1 || touches.Any(t => t.phase == TouchPhase.Moved))
-            {
+            if (touches.Count > 1 || touches.Any(t => t.phase == TouchPhase.Moved)) {
                 yield return StartCoroutine(HandleGesture());
                 break;
             }
@@ -37,10 +42,8 @@ public class TouchHandling : MonoBehaviour
         }
     }
 
-    private IEnumerator HandleGesture()
-    {
-        while (Input.touchCount > 0)
-        {
+    private IEnumerator HandleGesture() {
+        while (Input.touchCount > 0) {
             var touches = GetTouches();
 
             if (touches.Count == 1)
@@ -51,19 +54,75 @@ public class TouchHandling : MonoBehaviour
             yield return null;
         }
     }
+    #endregion
 
-    private void HandlePinch(List<Touch> touches)
-    {
-        throw new System.NotImplementedException();
+    #region callback handling for the different taps and gestures
+    private void HandlePinch(List<Touch> touches) {
+        // Right now we just do this, as we are pretty certain we only need one pinch handler,
+        // specifically for the camera. Keep in mind that we ignore UI touches, so UI pinches should still function.
+        _pinchHandler(touches);
     }
 
-    private void HandleSwipe(Touch touch)
-    {
-        throw new System.NotImplementedException();
+    private void HandleSwipe(Touch touch) {
+        // Right now we just do this, as we are pretty certain we only need one swipe handler,
+        // specifically for the camera. Keep in mind that we ignore UI touches, so UI swipes should still function.
+        _swipeHandler(touch);
     }
 
-    private void HandleTap(Touch tap)
-    {
-        throw new System.NotImplementedException();
+    private void HandleTap(Touch tap) {
+        // We get the handler for the type of gameobject we tapped based on its tag.
+        var ray = Camera.main.ScreenPointToRay(tap.position);
+        RaycastHit hit;
+        Action<RaycastHit> handler;
+        if (Physics.Raycast(ray, out hit) &&
+            _tapEventHandlers.TryGetValue(hit.collider.tag, out handler))
+                handler(hit);
     }
+    #endregion
+
+    #region registration of handlers
+    public void RegisterTapHandlerByTag(string tag, Action<RaycastHit> handler) {
+        // Overwrites old one if it exists
+        _tapEventHandlers[tag] = handler;
+    }
+
+    public void RegisterSwipeHandler(Action<Touch> handler) {
+        _swipeHandler = handler;
+    }
+
+    public void RegisterPinchHandler(Action<List<Touch>> handler) {
+        _pinchHandler = handler;
+    }
+    #endregion
+
+    #region removing handlers
+    public void ClearTapHandler(string tag) {
+        // Functions even if tag does not exist.
+        _tapEventHandlers.Remove(tag);
+    }
+
+    public void ClearAllTapHandlers() {
+        _tapEventHandlers.Clear();
+    }
+
+    public void ClearSwipeHandler() {
+        _swipeHandler = s => { };
+    }
+
+    public void ClearPinchHandler() {
+        _pinchHandler = p => { };
+    }
+
+    public void ClearAllHandlers() {
+        ClearAllTapHandlers();
+        ClearSwipeHandler();
+        ClearPinchHandler();
+    }
+    #endregion
+
+    #region private helpers
+    private static List<Touch> GetTouches() {
+        return Input.touches.Where(t => EventSystem.current.IsPointerOverGameObject(t.fingerId)).ToList();
+    }
+    #endregion
 }
