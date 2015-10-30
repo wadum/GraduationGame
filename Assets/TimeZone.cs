2a1c238913d2c4b1e90f6f8861e053b85c880592
Martin <kullax@feral.dk>
2015-10-30 08:48:03 +0100

    Yea, timezones should not still affect objects which leave them.

--- Assets/TimeZone.cs
+++ Assets/TimeZone.cs
@@ -15,6 +15,16 @@ public class TimeZone : MonoBehaviour {
 	
 	}
 
+    void OnTriggerExit(Collider collider)
+    {
+        TimeTrackable timescript = collider.GetComponent<TimeTrackable>();
+        if (timescript)
+        {
+            timescript.TimeMultiplier = 1f;
+        }
+
+    }
+
     void OnTriggerStay(Collider collider)
     {
         TimeTrackable timescript  = collider.GetComponent<TimeTrackable>();
