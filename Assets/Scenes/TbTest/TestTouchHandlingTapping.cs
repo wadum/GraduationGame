84efcf5df7e29981f5d3a283cbadc384f4fcae3b
Thorbjørn S. Kaiser <tsk@postme.com>
2015-11-04 10:27:25 +0100

    test scene updated with test script to test tap and hold

---
+++ Assets/Scenes/TbTest/TestTouchHandlingTapping.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(TouchHandling))]
+public class TestTouchHandlingTapping : MonoBehaviour {
+
+    public string TagToListenOn = "Player";
+
+    private TouchHandling _input;
+
+	// Use this for initialization
+	void Start () {
+	    _input = GetComponent<TouchHandling>();
+	    if (!_input) {
+	        Debug.Log("WHIIIIINE! NO INPUT SYYYYSSSSTEM!");
+	        enabled = false;
+	        return;
+	    }
+
+        _input.RegisterTapHandlerByTag(TagToListenOn, r => ExclaimTap(r.collider.tag));
+        _input.RegisterTapAndHoldHandlerByTag(TagToListenOn, r => ExclaimTapAndHold(r.collider.tag));
+	}
+
+    private void ExclaimTap(string objTag) {
+        Debug.Log("Did a tap on an object with tag " + objTag);
+    }
+	
+    private void ExclaimTapAndHold(string objTag) {
+        Debug.Log("Did a tap and hold on an object with tag " + objTag);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
