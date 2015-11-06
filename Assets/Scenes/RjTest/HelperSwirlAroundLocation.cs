01a4a07c80bd676768fdb71fc640b1466cce678f
RasmusJensen <nevatarion@hotmail.com>
2015-11-06 13:57:23 +0100

    Making The helper float to inteactable objects and adding target locations to prefabs

---
+++ Assets/Scenes/RjTest/HelperSwirlAroundLocation.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelperSwirlAroundLocation : MonoBehaviour {
+
+    public GameObject SwirlAroundLocation;
+    public float SwirlDistance;
+}
