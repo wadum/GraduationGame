180564e0d835aad10e4569cd2ec4c635f190a2a0
Thorbjørn S. Kaiser <tsk@postme.com>
2015-11-10 14:48:00 +0100

    working smoothing on fully automatic

--- Assets/Scripts/Camera/DynamicCamera.cs
+++ Assets/Scripts/Camera/DynamicCamera.cs
@@ -23,22 +23,7 @@ public class DynamicCamera : MonoBehaviour {
     private Collider _playerCollider;
 
     // Used to override the camera agent
-    private bool _playerZoomIntent;
-    private bool _playerRotateIntent;
-
-    // Used for smoothing
-    private float _desiredDistance;
-    private float _desiredZoom;
-    private float _desiredPitch;
-    private float _desiredYaw;
-
-    private Vector3 PlayerTop {
-        get {
-            return _playerCollider?
-                _player.position + new Vector3(0, _playerCollider.bounds.extents.y, 0):
-                _player.position;
-        }
-    }
+    private bool _playerIntent;
 
     private bool Sanity() {
         _camera = GetComponent<Camera>();
@@ -73,29 +58,38 @@ public class DynamicCamera : MonoBehaviour {
 
         MultiTouch.RegisterSwipeHandler(HandleSwipe);
 	    MultiTouch.RegisterPinchHandler(HandlePinch);
-        MultiTouch.RegisterTapHandlerByTag("Terrain", _ => _playerRotateIntent = _playerZoomIntent = false);
+        MultiTouch.RegisterTapHandlerByTag("Terrain", _ => _playerIntent = false);
 	}
 
     void LateUpdate() {
-        var baseYawAxis = _player.up;
-        var yaw = Quaternion.AngleAxis(NeutralYaw, baseYawAxis);
+        if (_playerIntent)
+            ConstrainedPlayerControl();
+        else
+            FullyAutomaticCameraControl();
+    }
 
-        var basePitchAxis = _player.right;
-        var pitchAxis = yaw * basePitchAxis;
-        var pitch = Quaternion.AngleAxis(NeutralPitch, pitchAxis);
+    private void ConstrainedPlayerControl() {
+        //transform.position = -transform.forward*NeutralDistanceFromPlayer + PlayerTop;
+    }
 
-        transform.position = pitch * yaw * (-_player.forward * NeutralDistanceFromPlayer) + PlayerTop;
+    private void FullyAutomaticCameraControl() {
+        var rot = Quaternion.RotateTowards(CurrentRelativeRotation, NeutralRotation, 1f);
+
+        Debug.Log(rot.eulerAngles);
+
+        SetRelativePosition(rot, NeutralDistanceFromPlayer);
+
         transform.LookAt(_player);
     }
 
     private void HandleSwipe(Touch swipe) {
-        _playerRotateIntent = true;
+        _playerIntent = true;
 
         var movementDirection = ScreenNormalize(swipe.deltaPosition);
     }
 
     private void HandlePinch(List<Touch> pinch) {
-        _playerZoomIntent = true;
+        _playerIntent = true;
 
         Func<List<Vector2>, Vector2> averagePoint = vectors => vectors.Aggregate((v1, v2) => v1 + v2) / vectors.Count;
         Func<List<Vector2>, Vector2, float> averageDistanceToPoint = (vectors, point) => vectors.Select(p => Vector2.Distance(p, point)).Average();
@@ -107,12 +101,58 @@ public class DynamicCamera : MonoBehaviour {
         var previousPositions = pinch.Select(t => ScreenNormalize(t.position - t.deltaPosition)).ToList();
         var previousCenter = averagePoint(previousPositions);
         var previousAverageDistanceToCenter = averageDistanceToPoint(previousPositions, previousCenter);
-
-        _desiredZoom += 2 * ZoomFovDegreesPerPixelPinch * (previousAverageDistanceToCenter - averageDistanceToCenter);
     }
 
     // Transforms v -> ([0, 1], [0, 1]) given v is in screen coordinates
     private static Vector2 ScreenNormalize(Vector2 v) {
         return new Vector2(v.x / Screen.width, v.y / Screen.height);
     }
+
+    #region Rotational Helpers
+    // Helper properties
+    private Vector3 RelativeYawAxis { get { return -_player.up; } }
+
+    private Vector3 RelativePitchAxis { get { return _player.right; } }
+
+    private Vector3 RelativePosition { get { return transform.position - PlayerTop; } }
+
+    private Quaternion NeutralRotation { get { return RelativeRotation(NeutralYaw, NeutralPitch); } }
+
+    private float CurrentDistance { get { return RelativePosition.magnitude; } }
+
+    private Quaternion CurrentRelativeRotation { get { return Quaternion.FromToRotation(-_player.forward, RelativePosition.normalized); } }
+
+    private float CurrentAbsoluteYaw {
+        get {
+            var yawComponent = Vector3.ProjectOnPlane(RelativePosition.normalized, Vector3.up);
+            var angle = Vector3.Angle(yawComponent, Vector3.forward);
+
+            return yawComponent.x > 0 ? angle : -angle;
+        }
+    }
+
+    private Vector3 PlayerTop {
+        get {
+            return _playerCollider?
+                _player.position + new Vector3(0, _playerCollider.bounds.extents.y, 0):
+                _player.position;
+        }
+    }
+
+    // Helper methods
+    private Quaternion RelativeRotation(float yaw, float pitch) {
+        var yawRot = Quaternion.AngleAxis(yaw, RelativeYawAxis);
+        var pitchRot = Quaternion.AngleAxis(pitch, yawRot*RelativePitchAxis);
+
+        return pitchRot*yawRot;
+    }
+
+    private void SetRelativePosition(float yaw, float pitch, float distance) {
+        SetRelativePosition(RelativeRotation(yaw, pitch), distance);
+    }
+
+    private void SetRelativePosition(Quaternion rotation, float distance) {
+        transform.position = rotation * (-_player.forward * distance) + PlayerTop;
+    }
+    #endregion
 }
