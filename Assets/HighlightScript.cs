ec0dd4195b5f113f949333e5471f953f1398a698
Martin <kullax@feral.dk>
2015-11-11 10:37:35 +0100

    Shader stuff

---
+++ Assets/HighlightScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightScript : MonoBehaviour {
+
+    Renderer rend;
+
+    void OnEnable()
+    {
+        rend = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseOver()
+    {
+        MasterHighlight master = transform.root.GetComponent<MasterHighlight>();
+        master.Activate();
+                rend.material.shader = Shader.Find("Outlined/Silhouetted Diffuse");
+    }
+
+    void OnMouseExit()
+    {
+        MasterHighlight master = transform.root.GetComponent<MasterHighlight>();
+        master.Deactivate();
+        rend.material.shader = Shader.Find("Diffuse");
+    }
+
+    public void Activate()
+    {
+        rend.material.shader = Shader.Find("Outlined/Silhouetted Diffuse");
+    }
+
+    public void Deactivate()
+    {
+        rend.material.shader = Shader.Find("Diffuse");
+    }
+}
---
+++ Assets/MasterHighlight.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MasterHighlight : MonoBehaviour {
+    List<HighlightScript> _list;
+
+	// Use this for initialization
+	void Start () {
+        _list = new List<HighlightScript>();
+        TagChildren(transform);
+    }
+
+    void TagChildren(Transform _transform)
+    {
+        foreach (Transform child in _transform.GetComponentInChildren<Transform>())
+        {
+            if (child.tag == "TimeManipulationObject")
+            {
+                var mesh = child.GetComponent<MeshRenderer>();
+                if (mesh)
+                {
+                    child.gameObject.AddComponent<HighlightScript>();
+                    _list.Add(child.GetComponent<HighlightScript>());
+                }
+                else
+                {
+                    TagChildren(child);
+                }
+            }
+        }
+    }
+
+    public void Activate()
+    {
+        foreach (HighlightScript script in _list)
+            script.Activate();
+    }
+
+    public void Deactivate()
+    {
+        foreach (HighlightScript script in _list)
+            script.Deactivate();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
