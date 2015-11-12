469456a807e82cb942c28c48e08fd8b6e2a7963f
RasmusJensen <nevatarion@hotmail.com>
2015-11-12 15:53:31 +0100

    piston

--- Assets/Scripts/Elevator/ElectrifiedElevator.cs
+++ Assets/Scripts/Elevator/ElectrifiedElevator.cs
@@ -8,22 +8,18 @@ public class ElectrifiedElevator : Electrified {
 
 	// Use this for initialization
 	void Start () {
-        StartCoroutine(_elevatorTrigger.gameObject.GetComponent<ElevatorTrigger>().moveElevator());
-        Ani.speed = 0;
         base.Deactivate();
 	}
 
     // Update is called once per frame
     override public bool Activate()
     {
-        Ani.speed = 1;
         //_elevatorTrigger.enabled = true;
         return base.Activate();
     }
 
     override public bool Deactivate()
     {
-        Ani.speed = 0;
         //_elevatorTrigger.enabled = false;
         return base.Deactivate();
     }
