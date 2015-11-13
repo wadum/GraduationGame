3d24d53349a00a1cee32f82b36edb0c7168a51f1
lemanou <lemanousakis@gmail.com>
2015-11-13 23:19:48 +0100

    IntroSceneDone

---
+++ Assets/Scripts/I18N/I18nCutscene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class I18nCutscene : MonoBehaviour
+{
+    public string Filename;
+    private Text _text;
+
+    // Use this for initialization
+    void Start()
+    {
+        _text = GetComponent<Text>();
+        _text.text = GetText();
+    }
+
+    private string GetText()
+    {
+        return Resources.Load<TextAsset>(
+            string.Format("Texts/{0}/{1}", I18n.GetInstance().GetLanguageName(), Filename))
+            .text;
+    }
+}
