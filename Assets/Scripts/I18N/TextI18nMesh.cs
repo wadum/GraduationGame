c48ae5ca50301bfa2bb4f94f844ed377eb7e6969
Ludwig <ludde.sandbacka@gmail.com>
2015-11-13 20:59:23 +0100

    Lighting and scene stuff

---
+++ Assets/Scripts/I18N/TextI18nMesh.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(TextMesh))]
+public class TextI18nMesh : MonoBehaviour {
+	
+	public string TranslationKey;
+	
+	private TextMesh text;
+	
+	void Start(){
+		text = GetComponentInParent<TextMesh>();
+		UpdateField ();
+	}
+	
+	public void UpdateField(){
+		if (I18n.GetInstance () != null && text != null) {
+			text.text = I18n.GetInstance ().Translate (TranslationKey);
+		}
+	}
+}
