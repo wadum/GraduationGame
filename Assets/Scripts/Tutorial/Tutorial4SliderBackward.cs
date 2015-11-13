2cd2e7525fa91df7e1323749b318765b40f4c2b0
ChristofferL <chrwadum@gmail.com>
2015-11-13 19:31:57 +0100

    New tutorial without lvl1 scene changes

---
+++ Assets/Scripts/Tutorial/Tutorial4SliderBackward.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Tutorial4SliderBackward : TutorialStep {
+
+	public GameObject Forward;
+	public GameObject Backward;
+
+	public Slider TimeSlider;
+
+	protected override bool Continue ()
+	{
+		return TimeSlider.value < 50;
+	}
+
+	override public IEnumerator Run()
+	{
+		TouchRequired = false;
+
+		Backward.GetComponent<EventTrigger>().enabled = true;
+		Backward.GetComponent<Button>().interactable = true;
+
+		yield return StartCoroutine(base.Run());
+
+		Backward.GetComponent<EventTrigger>().triggers.ForEach(t => {
+			if(t.eventID == EventTriggerType.PointerUp) 
+				t.callback.Invoke(new BaseEventData(GameObject.FindObjectOfType<EventSystem>()));
+			});
+		Backward.GetComponent<EventTrigger>().enabled = true;
+		Backward.GetComponent<Button>().interactable = true;
+		Forward.GetComponent<EventTrigger>().enabled = true;
+		Forward.GetComponent<Button>().interactable = true;
+		GameOverlayController.gameOverlayController.DeactivateSlider();
+	}
+}
