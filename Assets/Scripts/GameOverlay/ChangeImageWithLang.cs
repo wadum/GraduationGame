5c399f5b78ae0d91e2271ba3b25b4269deb215de
Eleftherios Manousakis <lemanousakis@gmail.com>
2015-11-15 10:29:42 +0100

    DanishControlsAdded

---
+++ Assets/Scripts/GameOverlay/ChangeImageWithLang.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeImageWithLang : MonoBehaviour
+{
+    public Image ThisImage;
+    public Sprite EnglishImage,
+        DanishImage;
+
+    void OnEnable()
+    {
+        if (I18n.GetInstance().GetLanguageName() == "English")
+            ThisImage.sprite = EnglishImage;
+        else
+            ThisImage.sprite = DanishImage;
+    }
+}
