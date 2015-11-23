6e4a0377886c91bbecc34d0d9906004a5739d8d5
RasmusJensen <nevatarion@hotmail.com>
2015-11-23 16:01:50 +0100

    Decals on floor when moving.

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class CharacterMovement : MonoBehaviour
 {
     public GameObject DeliverClockPartArea;
+    public GameObject PathMarker;
+    public Sprite DestinationMarker;
+    public float OptimalDistanceBetweenPathPoints = 2f;
     public float WaypointFloatSpeed = 1.0f;
     public float RotationSpeed = 3.0f;
     // The TutorialMoveFreeze is a way to freeze the player, ignoring selected input while true
@@ -23,6 +27,7 @@ public class CharacterMovement : MonoBehaviour
         {
             if (TutorialMoveFreeze) return;
             GoTo(hit.point);
+            StartCoroutine(DrawGreenHalos());
             GameOverlayController.gameOverlayController.DeactivateSlider();
         });
         MultiTouch.RegisterTapHandlerByTag("Clockpart", hit => {
@@ -69,8 +74,40 @@ public class CharacterMovement : MonoBehaviour
     {
         if (_agent.enabled)
         {
+            _agent.ResetPath();
             GameOverlayController.gameOverlayController.DeactivateSlider();
             _agent.SetDestination(position);
         }
+    }
+    IEnumerator DrawGreenHalos()
+    {
+        yield return new WaitForSeconds(0.1f);
+        // Start of: Creating Green halos
+        GameObject previous = null;
+        GameObject current;
+        for (int i = 1; i < _agent.path.corners.Length; i++)
+        {
+            float lerpFactor = 0.0f;
+            float distance = Vector3.Distance(_agent.path.corners[i], _agent.path.corners[i - 1]);
+            if (previous && Vector3.Distance(previous.transform.position, _agent.path.corners[i]) < OptimalDistanceBetweenPathPoints) continue;
+            lerpFactor = 1f/ (Mathf.Ceil((distance / OptimalDistanceBetweenPathPoints))+1f);
+
+            distance = lerpFactor;
+            while (distance < 1.1f)
+            {
+                Vector3 Pos = Vector3.Lerp(_agent.path.corners[i - 1], _agent.path.corners[i], distance);
+                current = (GameObject) Instantiate(PathMarker, Pos, Quaternion.identity);
+                distance += lerpFactor;
+                if (previous)
+                {
+                    previous.transform.LookAt(current.transform);
+                }
+                previous = current;
+            }
+
+        }
+
+        if(previous)
+            previous.GetComponentInChildren<SpriteRenderer>().sprite = DestinationMarker;
     }
 }
---
+++ Assets/Scripts/Character/GreenHaloController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GreenHaloController : MonoBehaviour {
+
+    NavMeshAgent _playerAgent;
+    Vector3 _finalPos = Vector3.zero;
+
+    void Update()
+    {
+        if (! _playerAgent)
+        {
+            _playerAgent = GameObject.FindGameObjectWithTag("Player").GetComponent<NavMeshAgent>();
+        }
+        if (_finalPos == Vector3.zero)
+        {
+            _finalPos = _playerAgent.destination;
+        }
+        if (_finalPos != _playerAgent.destination)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
