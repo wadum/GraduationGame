0ec19392cd383f879450b8e574d816fecad8c131
Martin <kullax@feral.dk>
2015-11-26 13:58:37 +0100

    Dissolving between levels.

---
+++ Assets/Dissolver.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dissolver : MonoBehaviour {
+    Renderer rend;
+    public float AmountOfTime = 2f;
+    private float _disAmount = 0, _timeAmound = 0;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        if (!rend)
+            Destroy(this);
+    }
+
+    public IEnumerator Dissolve()
+    {
+        _disAmount = 0;
+        _timeAmound = 0;
+        AmountOfTime = 2f;
+        while (_disAmount < 1)
+        {
+            rend.material.SetFloat("_DissolveAmount", _disAmount);
+            _timeAmound += Time.deltaTime;
+            _disAmount = _timeAmound / AmountOfTime;
+            yield return null;
+        }
+    }
+
+    public IEnumerator Resolve()
+    {
+        _disAmount = 0;
+        _timeAmound = 0;
+        AmountOfTime = 2f;
+        while (_disAmount < 1)
+        {
+            rend.material.SetFloat("_DissolveAmount", 1-_disAmount);
+            _timeAmound += Time.deltaTime;
+            _disAmount = _timeAmound / AmountOfTime;
+            yield return null;
+        }
+    }
+
+}
---
+++ Assets/MasterDissolver.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MasterDissolver : MonoBehaviour {
+    List<Dissolver> Dissolvers;
+	// Use this for initialization
+	void Start () {
+        Dissolvers = new List<Dissolver>();
+        Renderer[] rends = GameObject.FindObjectsOfType<Renderer>();
+        foreach(Renderer rend in rends)
+        {
+            if (rend.material.shader.name == "Shader Forge/ObejctDissolver")
+            {
+                Dissolvers.Add(rend.gameObject.AddComponent<Dissolver>());
+            }
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            foreach(Dissolver script in Dissolvers)
+                StartCoroutine(script.Dissolve());
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            foreach (Dissolver script in Dissolvers)
+                StartCoroutine(script.Resolve());
+        }
+    }
+
+}
