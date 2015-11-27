cfc56a7b7fcb6ea04d0b68e8ad1a7a7ea6f4a8e4
Thorbjørn S. Kaiser <tsk@postme.com>
2015-11-27 20:49:47 +0100

    resurrected editor properties - still needed for live editor feedback

---
+++ Assets/Editor/Custom.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(FogControl))]
+public class CustomInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        {
+            if (target.GetType() == typeof(FogControl))
+            {
+                var getterSetter = (FogControl)target;
+                getterSetter.colorProperty = getterSetter.color;
+                getterSetter.densityProperty = getterSetter.density;
+                getterSetter.ambientIntensityProperty = getterSetter.ambientIntensity;
+            }
+        }
+    }
+}
