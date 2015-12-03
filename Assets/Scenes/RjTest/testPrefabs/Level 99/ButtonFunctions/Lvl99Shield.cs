b2c4cc8213416b9a80338e8c7fda5377b7bc6327
RasmusJensen <nevatarion@hotmail.com>
2015-12-03 21:24:50 +0100

    Credits sceneFix

---
+++ Assets/Scenes/RjTest/testPrefabs/Level 99/ButtonFunctions/Lvl99Shield.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lvl99Shield : MonoBehaviour {
+
+    public static bool Shield;
+    public lvl99ShieldController Level99Shield;
+
+    public void StartShield()
+    {
+        Shield = true;
+        StartCoroutine(ShieldDuration());
+    }
+    IEnumerator ShieldDuration()
+    {
+        Level99Shield.ActivateShield();
+        yield return new WaitForSeconds(Level99UIController.ShieldLevel);
+        Level99Shield.DeactivateShield();
+    }
+}
--- Assets/Scenes/RjTest/testPrefabs/Level 99/Level99UIController.cs
+++ Assets/Scenes/RjTest/testPrefabs/Level 99/Level99UIController.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class Level99UIController : MonoBehaviour {
 
+    //ChainLigtning
     public static int ChainLevel = 2;
+    //Shield duration
+    public static float ShieldLevel = 10;
 
     public GameObject DeathScreen;
     public GameObject PowerUpChoosinScreen;
