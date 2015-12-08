909f64238979c9d51ce45483fe32624872067a15
Thorbjørn S. Kaiser <lazareth@gmail.com>
2015-12-08 19:10:52 +0100

    made rotation speed of receivers framerate independant.

--- Assets/Scripts/Bridge/WindmillRotator.cs
+++ Assets/Scripts/Bridge/WindmillRotator.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Linq;
-using System.Collections;
 using System.Collections.Generic;
 
 public class WindmillRotator : MonoBehaviour {
@@ -14,32 +13,35 @@ public class WindmillRotator : MonoBehaviour {
 	void Awake () {
         On = transform.root.GetComponentsInChildren<Electrified>();
 	}
-	Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
-    {
-        Vector3 dir = point - pivot;
-        dir = Quaternion.Euler(angles) * dir;
-        point = dir + pivot;
-        return point;
+
+    private static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles) {
+        return Quaternion.Euler(angles) * (point - pivot) + pivot;
     }
+
+    private static Vector3 RotateObjectAroundObject(GameObject rotatee, GameObject rotator, Vector3 rotation) {
+        return RotatePointAroundPivot(rotatee.transform.position, rotator.transform.position, rotation);
+    }
+
 	void Update () {
-        if (On.Any(b => b.Active))
-        {
-
-            if (ObjectsToRotate.Count > 0)
-        {
-                foreach (GameObject obj in ObjectsToRotate)
-                {
-                    if(obj != CubeIcon)
-                        obj.transform.position = RotatePointAroundPivot(obj.transform.position, RotateObject.transform.position, new Vector3(0, 0, RotationSpeed));
-                    obj.transform.Rotate(new Vector3(0, 0, RotationSpeed));
-                }
-//                    obj.transform.Rotate()RotateAround(RotateObject.transform.position, RotationSpeed);
+	    if (!On.Any(b => b.Active))
             return;
-        }
 
-//        if (On.Any(b => b.Active))
-  //      {
-            RotateObject.transform.Rotate(new Vector3(0, 0, RotationSpeed));
-        }
-    }
+        // Making the rotation framerate independant by adding Time.deltaTime means all calibrations in the levels are off.
+        // TODO: This constant should be removed, but it requires someone to go through and correct all the rotators in all the levels.
+	    const float legacyAdjustment = 40f;
+        var rotation = new Vector3(0, 0, RotationSpeed * Time.deltaTime * legacyAdjustment);
+
+	    if (ObjectsToRotate.Count > 0) {
+	        foreach (var obj in ObjectsToRotate) {
+	            if(obj != CubeIcon)
+	                obj.transform.position = RotateObjectAroundObject(obj, RotateObject, rotation);
+
+	            obj.transform.Rotate(rotation);
+	        }
+
+	        return;
+	    }
+
+	    RotateObject.transform.Rotate(rotation);
+	}
 }
